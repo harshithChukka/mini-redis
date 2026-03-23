e03a1525ed8c7915b41f43e6a8c3e3f5f67bdceb
harshith-chukka <harshith.v@rently.com>
2026-03-23 22:12:29 +0530

    added session module

---
+++ include/miniredis/session.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <string>
+#include <deque>
+#include <mutex>
+
+#include "miniredis/resp_parser.hpp"
+#include "miniredis/command_executor.hpp"
+
+using boost::asio::ip::tcp;
+
+class Session : public std::enable_shared_from_this<Session> {
+public:
+  Session(boost::asio::io_context& io) : socket_(io) {};
+  auto& socket() { return socket_; }
+
+  void start();
+
+private:
+  void doRead();
+  void doWrite(const std::string&);
+  void handleCommand(const size_t&);
+  void handleError(const boost::system::error_code&);
+  void writeNext();
+
+  tcp::socket socket_;
+  boost::asio::streambuf buffer_;
+  RespParser parser_;
+  CommandExecutor executor_;
+  bool isWriteInProgress = false;
+  std::deque<std::string> writeQueue_;
+  std::mutex writeMutex_;  
+};
