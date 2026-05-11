f8601d28e17395a4e8ebed22d62fdf98d097117e
harshith-chukka <harshith.v@rently.com>
2026-05-11 23:24:25 +0530

    added support for PING and INCR commands

--- include/miniredis/database.hpp
+++ include/miniredis/database.hpp
@@ -4,12 +4,14 @@
 #include <unordered_map>
 #include <shared_mutex>
 #include <memory>
+#include <optional>
 
 class Database {
 public:
     void set(const std::string& key, const std::string& value);
     std::shared_ptr<std::string> get(const std::string& key);
     int del(const std::string& key);
+    std::optional<int64_t> incr(const std::string& key);
 
 private:
     std::unordered_map<std::string, std::string> store_;
