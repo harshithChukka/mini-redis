7cafe89c77135cc419ff5c94f0515869bcadb808
harshithChukka <harshith2209@gmail.com>
2026-05-11 23:24:25 +0530

    added support for PING and INCR commands

--- src/database.cpp
+++ src/database.cpp
@@ -1,5 +1,22 @@
 #include "miniredis/database.hpp"
 
+namespace {
+
+std::optional<int64_t> convertToInt(const std::string& s) {
+  try {
+    size_t pos;
+    int64_t value = std::stoll(s, &pos);
+    if (pos == s.length()) 
+      return value;
+    return std::nullopt;
+  }
+  catch (...) {
+    return  std::nullopt;
+  }
+}
+
+}
+
 void Database::set(const std::string& key, const std::string& value) {
   std::lock_guard<std::shared_mutex> lock(mutex_);
   store_[key] = value;
@@ -21,4 +38,21 @@ int Database::del(const std::string& key) {
     return 0;
   store_.erase(key);
   return 1;
+}
+
+std::optional<int64_t> Database::incr(const std::string& key) {
+  std::lock_guard<std::shared_mutex> lock(mutex_);
+  auto it = store_.find(key);
+  if (it == store_.end()) {
+    store_[key] = std::to_string(1);
+    return 1;
+  }
+
+  auto value = convertToInt(it->second);
+  if (!value)
+    return std::nullopt;
+
+  ++(*value);
+  it->second = std::to_string(*value);
+  return *value;
 }
